49acb7b633b6add2b30d580ffa73d3c17ce8daec
Christin <c.albrecht289@gmail.com>
2025-09-10 12:58:40 +0200

    Add filter_users_by_age function and integrate into script

--- filter_users.py
+++ filter_users.py
@@ -2,26 +2,53 @@ import json
 
 
 def filter_users_by_name(name):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    """Filters and prints users by name."""
+    try:
+        with open("users.json", "r") as f:
+            users = json.load(f)
+    except FileNotFoundError:
+        print("File: users.json not found")
+        return
 
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
-    for user in filtered_users:
-        print(user)
+    if not filtered_users:
+        print(f"No users found with the name '{name}'.")
+    else:
+        for user in filtered_users:
+            print(user)
+
+
+def filter_users_by_age(age):
+    """Filters and prints users by age."""
+    try:
+        with open("users.json", "r") as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        print("File: users.json not found")
+        return
+
+    filtered_users = [user for user in users if user["age"] == age]
+
+    if not filtered_users:
+        print(f"No users found with the age '{age}'.")
+    else:
+        for user in filtered_users:
+            print(user)
 
 
 if __name__ == "__main__":
     filter_option = (
-        input(
-            "What would you like to filter by? (Currently, only 'name' is supported): "
-        )
-        .strip()
-        .lower()
+        input("What would you like to filter by (name or age): ").strip().lower()
     )
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+
+    elif filter_option == "age":
+        age_to_search = int(input("Enter an age to filter users: "))
+        filter_users_by_age(age_to_search)
+
     else:
         print("Filtering by that option is not yet supported.")
